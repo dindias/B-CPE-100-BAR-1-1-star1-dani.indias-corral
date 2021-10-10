d51dc803ff295bd2f8e9f1bc99ca727d824c1b7d
dindias <dani.indias-corral@epitech.eu>
2021-10-10 23:50:58 +0200

    side added

--- star.c
+++ star.c
@@ -18,7 +18,7 @@ int top (int x)
 {
     int i = 0;
     int j = 0;
-    int margin = (x*3);
+    int margin = x*3;
 
     while (i <= x-1) {
         while (j <= margin + i) {
@@ -36,9 +36,62 @@ int top (int x)
     return(x);
 }
 
+int vertical_side (int x)
+{
+    int i = 0;
+    int j = 0;
+    int margin = (x*2)+1;
+
+    while (i <= x) {
+        while (j <= margin){
+            if (i == 0){
+                my_putchar(42);
+            } else {
+                if (i == j) {
+                    my_putchar(42);
+                } else {
+                    my_putchar(32);
+                }
+            }
+            j++;
+        }
+        if (i != x) {
+            my_putchar('\n');
+        }
+        j = 0;
+        i++;
+    }
+    i = 0;
+    j = x;
+    while (i <= x) {
+        while (j > 0){
+            if (i == 0 && j == x + 1){
+                my_putchar(42);
+            } else {
+                if (i == j) {
+                    my_putchar(42);
+                } else {
+                    my_putchar(32);
+                }
+            }
+            j--;
+        }
+        if (i != x) {
+            my_putchar('\n');
+        } else {
+            my_putchar(42);
+        }
+        j = x;
+        i++;
+    }
+
+    return(x);
+}
+
 void star(int x)
 {
     top(x);
+    vertical_side(x);
 }
 
 int main()
